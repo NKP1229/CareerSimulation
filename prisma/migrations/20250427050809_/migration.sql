06299a6e004d6876eb50adda29f7c15a8c37cf51
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-27 01:09:56 -0400

    added prisma schema & migration

---
+++ prisma/migrations/20250427050809_/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Item" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "review" TEXT,
+    "comment" TEXT,
+
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+);
