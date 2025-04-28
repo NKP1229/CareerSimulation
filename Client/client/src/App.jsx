8c2b763162ebc37b0ff46df46dd2226c9b663a38
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-28 18:06:52 -0400

    all sanwiches show on home page

--- Client/client/src/App.jsx
+++ Client/client/src/App.jsx
@@ -1,10 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch("/api/items");
+        if (!response.ok) {
+          throw new Error("Failed to fetch items");
+        }
+        const json = await response.json();
+        setItems(json.response);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+    fetchItems();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
-      <h1>Welcome to the Site!</h1>
+      <nav>
+        <ul>
+          <li>
+            <button>Home</button>
+          </li>
+          <li>
+            <button>SignUp</button>
+          </li>
+          <li>
+            <button>LogIn</button>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>Sandwich menu:</h1>
+        <section>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
+                <button>{item.name}</button>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </main>
     </>
   );
 }
